3b1011da66e6a3ff70647d94dd993906755e44c3
Matthew Brown <matthewbrown0798@gmail.com>
2020-04-24 11:21:39 -0400

    added instructions for app

--- src/components/playlist.js
+++ src/components/playlist.js
@@ -29,7 +29,17 @@ export default function Playlist({
           ></iframe>
         </div>
       ) : (
-        <div></div>
+        <div
+          style={{ paddingTop: "10vh", fontSize: "5vh", textAlign: "center" }}
+        >
+          <div style={{ paddingTop: "3vh" }}>1. Enter Up To Five Artists</div>
+          <div style={{ paddingTop: "3vh" }}>
+            2. Set Preferred Levels for Musical Inputs
+          </div>
+          <div style={{ paddingTop: "3vh" }}> 3. Generate</div>
+
+          <div style={{ paddingTop: "3vh" }}>4. Enjoy a Variant Playlist</div>
+        </div>
       )}
     </div>
   );
